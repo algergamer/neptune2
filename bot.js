de2ead4dfcdd266a2e6608205b9d8a9bb1474af9
algergamer <43455217+algergamer@users.noreply.github.com>
2018-11-10 20:26:37 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2046,7 +2046,7 @@ if (message.mentions.users.size < 1) return message.reply("**منشن الشخص
  
 client.on("ready", () => {
  function arabecodes() {
-   client.guilds.get('503179541468938250').roles.find("name", "Neptune").setColor("RANDOM");
+   client.guilds.get('494893307223080992')roles.find("name", "MARIAM").setColor("RANDOM");
  }
  setInterval(arabecodes, 1700);
 })
